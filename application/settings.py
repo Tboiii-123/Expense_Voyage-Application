62ff6ba2eb1279255762a78b24100b57636e03da
Lawal Hussein <132585505+Tboiii-123@users.noreply.github.com>
2024-10-26 10:58:37 +0000

    Update settings.py

--- application/settings.py
+++ application/settings.py
@@ -81,24 +81,24 @@ WSGI_APPLICATION = 'application.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'trip_sp5y',
+         'USER': 'trip_sp5y_user',
+         'PASSWORD': 'MvkZ0wXrIz3Mj6iCGFcip0tnMvEqEpOV',
+         'HOST': 'dpg-cs4rmf5umphs73ajlf0g-a.oregon-postgres.render.com',
+         'PORT': '5432',
+     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'trip_sp5y',
-#         'USER': 'trip_sp5y_user',
-#         'PASSWORD': 'MvkZ0wXrIz3Mj6iCGFcip0tnMvEqEpOV',
-#         'HOST': 'dpg-cs4rmf5umphs73ajlf0g-a.oregon-postgres.render.com',
-#         'PORT': '5432',
-#     }
-# }
 
 
 
